b39522c862911ffddb21d562f51072d5ade59c48
Antonio Rondán <kaysera0@icloud.com>
2023-09-21 19:00:00 +0200

    arreglado bug en formulario de reset pwd

--- src/forms/password-reset-form.jsx
+++ src/forms/password-reset-form.jsx
@@ -1,26 +1,27 @@
 import { TextField, Button } from "@mui/material";
 
 export const PasswordResetForm = ({
-  setFormData,
-  handleInputChange,
-  handleSubmit,
-  validationErrors,
+	setFormData,
+	handleInputChange,
+	handleSubmit,
+	validationErrors,
 }) => (
-  <form
-    onSubmit={handleSubmit}
-    className="flex flex-col gap-4 md:px-44 lg:px-60 xl:px-96"
-  >
-    <TextField
-      label="Correo electrónico"
-      type="email"
-      value={setFormData}
-      onChange={handleInputChange}
-      required
-      error={Boolean(validationErrors)}
-      helperText={validationErrors}
-    />
-    <Button type="submit" variant="contained" color="primary">
-      Enviar código de recuperación
-    </Button>
-  </form>
+	<form
+		onSubmit={handleSubmit}
+		className="flex flex-col gap-4 md:px-44 lg:px-60 xl:px-96"
+	>
+		<TextField
+			label="Correo electrónico"
+			type="email"
+			name="email"
+			value={setFormData}
+			onChange={handleInputChange}
+			required
+			error={Boolean(validationErrors)}
+			helperText={validationErrors}
+		/>
+		<Button type="submit" variant="contained" color="primary">
+			Enviar código de recuperación
+		</Button>
+	</form>
 );
--- src/pages/recovery-password.jsx
+++ src/pages/recovery-password.jsx
@@ -7,74 +7,73 @@ import { PasswordResetForm } from "../forms/password-reset-form";
 import { toast } from "sonner";
 
 export function PasswordResetRequest() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const [formData, setFormData] = useState({ email: "" });
+	const [formData, setFormData] = useState({ email: "" });
+	const [validationErrors, setValidationErrors] = useState({});
 
-  const [validationErrors, setValidationErrors] = useState({});
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({
+			...formData,
+			[name]: value,
+		});
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+		const validationError = validateField(name, value, passwordResetSchema);
+		setValidationErrors({
+			...validationErrors,
+			[name]: validationError,
+		});
+	};
 
-    const validationError = validateField(name, value, passwordResetSchema);
-    setValidationErrors({
-      ...validationErrors,
-      [name]: validationError,
-    });
-  };
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+		const { error } = passwordResetSchema.validate(formData, {
+			abortEarly: false,
+		});
 
-    const { error } = passwordResetSchema.validate(formData, {
-      abortEarly: false,
-    });
+		if (error) {
+			const errors = {};
+			error.details.forEach((detail) => {
+				errors[detail.path[0]] = detail.message;
+			});
+			setValidationErrors(errors);
+			return;
+		}
 
-    if (error) {
-      const errors = {};
-      error.details.forEach((detail) => {
-        errors[detail.path[0]] = detail.message;
-      });
-      setValidationErrors(errors);
-      return;
-    }
+		const emailExists = await checkEmail(formData.email);
+		if (emailExists) {
+			try {
+				const resetSuccessful = await resetPassword(formData.email);
+				if (resetSuccessful) {
+					toast.success(
+						"El código de recuperación se ha enviado correctamente."
+					);
+					navigate("/users/change-password");
+				} else {
+					toast.error("Error al enviar el código de recuperación.");
+				}
+			} catch (error) {
+				console.error(error);
+				toast.error("Ocurrió un error al enviar el código de recuperación.");
+			}
+		} else {
+			toast.error(
+				"El correo electrónico no está registrado en nuestra base de datos."
+			);
+		}
+	};
 
-    const emailExists = await checkEmail(formData.email);
-    if (emailExists) {
-      try {
-        const resetSuccessful = await resetPassword(formData.email);
-        if (resetSuccessful) {
-          toast.success(
-            "El código de recuperación se ha enviado correctamente."
-          );
-          navigate("/users/change-password");
-        } else {
-          toast.error("Error al enviar el código de recuperación.");
-        }
-      } catch (error) {
-        console.error(error);
-        toast.error("Ocurrió un error al enviar el código de recuperación.");
-      }
-    } else {
-      toast.error(
-        "El correo electrónico no está registrado en nuestra base de datos."
-      );
-    }
-  };
-
-  return (
-    <Main>
-      <h1 className="text-4xl block self-center">Recupera tu contraseña</h1>
-      <PasswordResetForm
-        formData={setFormData}
-        handleInputChange={handleInputChange}
-        handleSubmit={handleSubmit}
-        error={validationErrors}
-      />
-    </Main>
-  );
+	return (
+		<Main>
+			<h1 className="text-4xl block self-center">Recupera tu contraseña</h1>
+			<PasswordResetForm
+				formData={setFormData}
+				handleInputChange={handleInputChange}
+				handleSubmit={handleSubmit}
+				error={validationErrors}
+			/>
+		</Main>
+	);
 }
