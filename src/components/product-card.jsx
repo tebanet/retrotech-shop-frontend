6255a8e8717618e0b352fd0898cbcddb665d63d7
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2023-09-25 20:44:36 +0200

    arreglados botones

--- src/components/product-card.jsx
+++ src/components/product-card.jsx
@@ -2,20 +2,20 @@ import { Link } from "react-router-dom";
 import { API_HOST } from "../utils/constants";
 
 export function ProductCard({ product }) {
-  return (
-    <article className="flex w-[10.75rem] h-52 md:h-60">
-      <section className="flex flex-col h-max">
-        <Link to={"/products/" + product.product_id}>
-          <img
-            src={API_HOST + "/uploads/" + product.product_image}
-            alt={"Picture of " + product.product_title}
-          />
-        </Link>
-        <h2 className="text-[var(--quaternary-color)]">{product.price}</h2>
-        <p className="text-[var(--quaternary-color)]">
-          {product.product_title}
-        </p>
-      </section>
-    </article>
-  );
+	return (
+		<article className="flex w-[10.75rem] h-52 md:h-60">
+			<section className="flex flex-col h-max">
+				<Link to={"/products/" + product.product_id}>
+					<img
+						src={product.product_image}
+						alt={"Picture of " + product.product_title}
+					/>
+				</Link>
+				<h2 className="text-[var(--quaternary-color)]">{product.price}</h2>
+				<p className="text-[var(--quaternary-color)]">
+					{product.product_title}
+				</p>
+			</section>
+		</article>
+	);
 }
