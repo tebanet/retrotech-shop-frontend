899f70af35912266d1aab6b4a4dce9b6670c4cec
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2023-10-02 10:30:56 +0200

    Cambio en login

--- src/api/post-login-request.js
+++ src/api/post-login-request.js
@@ -1,27 +1,46 @@
 import { API_HOST } from "../utils/constants";
 import { toast } from "sonner";
+import { METHODS, sendApiRequest } from "./send-api-request";
 
-export const loginUser = async (email, password) => {
-  const requestBody = { email, password };
+/* export const loginUser = async (email, password) => {
+	const requestBody = { email, password };
 
-  try {
-    const response = await fetch(API_HOST + "/users/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(requestBody),
-    });
+	try {
+		const response = await fetch(API_HOST + "/users/login", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(requestBody),
+		});
 
-    if (response.ok) {
-      const responseData = await response.json();
-      toast.success("Bienvenido a RetroTechShop");
-      return responseData.token;
-    } else {
-      toast.error("Usuario o contraseña incorrectos");
-      console.log("Error:", response.statusText);
-      return null;
-    }
-  } catch (error) {
-    console.error("Ha ocurrido un error:", error);
-    return null;
-  }
-};
+		if (response.ok) {
+			const responseData = await response.json();
+			toast.success("Bienvenido a RetroTechShop");
+			return responseData.token;
+		} else {
+			toast.error("Usuario o contraseña incorrectos");
+			console.log("Error:", response.statusText);
+			return null;
+		}
+	} catch (error) {
+		console.error("Ha ocurrido un error:", error);
+		return null;
+	}
+}; */
+
+export async function loginUser(email, password) {
+	const requestObject = { email, password };
+	const response = await sendApiRequest(
+		METHODS.POST,
+		"/users/login",
+		requestObject
+	);
+	console.log(response);
+	if (response.status === "ok") {
+		toast.success("Bienvenido a RetroTechShop");
+		return response.token;
+	} else {
+		toast.error("Usuario o contraseña incorrectos");
+		console.log("Error:", response.statusText);
+		return null;
+	}
+}
--- src/pages/login-page.jsx
+++ src/pages/login-page.jsx
@@ -8,75 +8,75 @@ import { loginUser } from "../api/post-login-request";
 import { LoginForm } from "../forms/login-form";
 
 export function LoginUserPage() {
-  const setCurrentUserToken = useLogin();
-  const navigate = useNavigate();
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
+	const setCurrentUserToken = useLogin();
+	const navigate = useNavigate();
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
 
-  const [validationErrors, setValidationErrors] = useState({});
+	const [validationErrors, setValidationErrors] = useState({});
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({
+			...formData,
+			[name]: value,
+		});
 
-    const validationError = validateField(name, value, loginUserSchema);
-    setValidationErrors({
-      ...validationErrors,
-      [name]: validationError,
-    });
-  };
+		const validationError = validateField(name, value, loginUserSchema);
+		setValidationErrors({
+			...validationErrors,
+			[name]: validationError,
+		});
+	};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    const { error } = loginUserSchema.validate(formData, { abortEarly: false });
+		const { error } = loginUserSchema.validate(formData, { abortEarly: false });
 
-    if (error) {
-      const errors = {};
-      error.details.forEach((detail) => {
-        errors[detail.path[0]] = detail.message;
-      });
-      setValidationErrors(errors);
-      return;
-    }
+		if (error) {
+			const errors = {};
+			error.details.forEach((detail) => {
+				errors[detail.path[0]] = detail.message;
+			});
+			setValidationErrors(errors);
+			return;
+		}
 
-    const token = await loginUser(formData.email, formData.password);
-    if (token) {
-      setCurrentUserToken(token);
-      navigate("/");
-    } else {
-      setFormData({ email: "", password: "" });
-    }
-  };
+		const token = await loginUser(formData.email, formData.password);
+		if (token) {
+			setCurrentUserToken(token);
+			navigate("/");
+		} else {
+			setFormData({ email: "", password: "" });
+		}
+	};
 
-  return (
-    <Main>
-      <h1 className="text-4xl block self-center">Inicia sesión</h1>
-      <LoginForm
-        formData={formData}
-        handleInputChange={handleInputChange}
-        validationErrors={validationErrors}
-        handleSubmit={handleSubmit}
-      />
-      <p className="flex justify-center gap-2">
-        ¿No tienes cuenta?
-        <Link to="/register" style={{ color: "var(--quaternary-color)" }}>
-          ¡Regístrate!
-        </Link>
-      </p>
-      <p className="flex justify-center gap-2">
-        <Link
-          to="/users/recovery-password"
-          style={{ color: "var(--quaternary-color)" }}
-        >
-          ¿Olvidaste tu contraseña?
-        </Link>
-      </p>
-    </Main>
-  );
+	return (
+		<Main>
+			<h1 className="text-4xl block self-center">Inicia sesión</h1>
+			<LoginForm
+				formData={formData}
+				handleInputChange={handleInputChange}
+				validationErrors={validationErrors}
+				handleSubmit={handleSubmit}
+			/>
+			<p className="flex justify-center gap-2">
+				¿No tienes cuenta?
+				<Link to="/register" style={{ color: "var(--quaternary-color)" }}>
+					¡Regístrate!
+				</Link>
+			</p>
+			<p className="flex justify-center gap-2">
+				<Link
+					to="/users/recovery-password"
+					style={{ color: "var(--quaternary-color)" }}
+				>
+					¿Olvidaste tu contraseña?
+				</Link>
+			</p>
+		</Main>
+	);
 }
