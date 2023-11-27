026b9370772685073996d48efaadd9a2bad10c31
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2023-11-27 16:25:21 +0100

    back deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const CURRENT_USER_STORAGE_ID = "userToken";
 
-//export const API_HOST = "https://retrotechshop.alwaysdata.net";
+export const API_HOST = "http://retrotech-shop.alwaysdata.net";
 
-export const API_HOST = "http://localhost:3000";
+//export const API_HOST = "http://localhost:3000";
